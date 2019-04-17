1bb8f03b5a622f391812e7758cf3bc4c3a8bc20b
CRjulianTICO <crjuliantico@gmail.com>
2019-04-17 13:04:20 -0600

    Add project files.

---
+++ Proyecto_MongoDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_MongoDB.Startup))]
+namespace Proyecto_MongoDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
