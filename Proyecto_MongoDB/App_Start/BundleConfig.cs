8cc938013c8638fbb0a87cc0c22157ed51bcfbda
AlfredoJHN <41807769+AlfredoJHN@users.noreply.github.com>
2019-05-01 13:52:23 -0600

    Template

--- Proyecto_MongoDB/App_Start/BundleConfig.cs
+++ Proyecto_MongoDB/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Proyecto_MongoDB
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-flatly.css",
                       "~/Content/site.css"));
         }
     }
